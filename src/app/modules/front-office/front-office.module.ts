15659fcd82015cca281473daded6f3cf909cd37a
SabrinaKaren <sabrinaa.karen@hotmail.com>
2023-02-15 14:32:54 -0300

    front-office: retirada do import do accordion (para ser feita em apresentação)

--- src/app/modules/front-office/front-office.module.ts
+++ src/app/modules/front-office/front-office.module.ts
@@ -2,7 +2,6 @@ import { SharedModule } from './../../shared/shared.module';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FrontOfficeComponent } from './front-office.component';
-import { AccordionModule } from 'primeng/accordion';
 
 @NgModule({
   declarations: [
@@ -10,8 +9,7 @@ import { AccordionModule } from 'primeng/accordion';
   ],
   imports: [
     CommonModule,
-    SharedModule,
-    AccordionModule
+    SharedModule
   ]
 })
 export class FrontOfficeModule { }
